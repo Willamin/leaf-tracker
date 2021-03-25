2e08bd00f582ff314e076fba1bd91663785e3c37
Will Lewis <will@wflewis.com>
2021-03-25 10:32:16 -0400

    Add seeds and update readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Project.create([{name: 'Foo'}, {name: 'Bar'}])
+aoc = Project.create({name: "Advent of Code 2020", status: "unstarted"})
+
+25.times do |i|
+  Task.create({title: "Day #{i} Challenge", description: "", status: "unstarted", project_id: aoc.id})
+end
+
+client = Project.create(name: "Insurance Client")
+Task.create(project_id: client.id, title: "Implement eligibility verification", description: "", status: "started")
+Task.create(project_id: client.id, title: "Implement submitting a claim", description: "", status: "unstarted")
+Task.create(project_id: client.id, title: "Send notification email to user when claim is accepted", description: "", status: "unstarted")
+Task.create(project_id: client.id, title: "Send notification email to user when claim is rejected", description: "", status: "unstarted")
+Task.create(project_id: client.id, title: "Display claim status to user", description: "", status: "unstarted")
